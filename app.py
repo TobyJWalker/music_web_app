d3197e55fb4f3ceffed5414d4a09fb0fb50494a8
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-09 13:01:02 +0100

    Test driven album repository with web app

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 import os
 from flask import Flask, request
+from lib.database_connection import get_flask_database_connection
+from lib.album_repository import *
 
 # Create a new Flask app
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/albums', methods=['GET'])
+def get_all_albums():
+    conn = get_flask_database_connection(app)
+    repo = AlbumRepository(conn)
+
+    albums = repo.all()
+
+    return '\n'.join([str(album) for album in albums])
+
+@app.route('/albums/<id>', methods=['GET'])
+def get_one_album(id):
+    conn = get_flask_database_connection(app)
+    repo = AlbumRepository(conn)
+
+    return str(repo.find(id))
+
+@app.route('/albums', methods=['POST'])
+def create_new_album():
+    conn = get_flask_database_connection(app)
+    repo = AlbumRepository(conn)
+
+    title = request.form['title']
+    release_year = int(request.form['release_year'])
+    artist_id = int(request.form['artist_id'])
+
+    repo.create(title, release_year, artist_id)
+    return '200'
 
 
 # These lines start the server if you run this file directly
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,30 @@
+from lib.album_repository import *
+
 # Tests for your routes go here
 
+def test_list_all_albums(web_client, db_connection):
+    db_connection.seed('seeds/music_library.sql')
+    response = web_client.get('/albums')
+
+    assert response.status_code == 200
+    assert 'Album(1, Doolittle, 1989, 1)' in response.data.decode('utf-8')
+
+def test_create_album(web_client, db_connection):
+    db_connection.seed('seeds/music_library.sql')
+
+    response = web_client.post('/albums', data={
+        'title': 'Voyage',
+        'release_year': 2022,
+        'artist_id': 2
+    })
+
+    assert response.status_code == 200
+    assert 'Voyage' in web_client.get('/albums').data.decode()
+
+def test_get_one_album(web_client, db_connection):
+    db_connection.seed('seeds/music_library.sql')
+
+    response = web_client.get('/albums/1')
+
+    assert response.status_code == 200
+    assert response.data.decode() == 'Album(1, Doolittle, 1989, 1)'
